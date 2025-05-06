88e7deb004a7d926b71407baf23b746e9d2bf12e
Mahidul Islam <mahidulislamtamim@gmail.com>
2025-05-06 12:46:00 +0600

    Remove existing files

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ from pdf2docx import Converter
 import os
 
 app = Flask(__name__)
+
+def clear_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
+
+# Define folders 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 upload_folder = os.path.join(script_dir, "upload")
 output_folder = os.path.join(script_dir, "converted")
 
+# Clear folder
+clear_folder(upload_folder)
+clear_folder(output_folder)
+
+# Create folder
 os.makedirs(upload_folder, exist_ok=True)
 os.makedirs(output_folder, exist_ok=True)
 
